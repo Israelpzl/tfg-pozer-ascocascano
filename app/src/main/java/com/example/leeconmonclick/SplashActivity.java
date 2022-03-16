f43b14c5c5de65f11b0d45f05643b16a3e5fda07
Yeray <y.cornejo.2018@alumnos.urjc.es>
2022-03-16 19:24:30 +0100

    Add of SplashActivity and ErrorActivity

--- app/src/main/java/com/example/leeconmonclick/SplashActivity.java
+++ app/src/main/java/com/example/leeconmonclick/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.leeconmonclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //Convertir a pantalla completa
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);//abre la StartActivity tras 3 segundos
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class/*cambiar a la de home*/));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        thread.start();
     }
 }

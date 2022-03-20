cb398638e7873dd763af392a3447f134fe2dea8f
Yeray <y.cornejo.2018@alumnos.urjc.es>
2022-03-20 15:14:52 +0100

    Update users screen

--- app/src/main/java/com/example/leeconmonclick/ProfilesActivity.java
+++ app/src/main/java/com/example/leeconmonclick/ProfilesActivity.java
@@ -2,8 +2,10 @@ package com.example.leeconmonclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfilesActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class ProfilesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profiles);
+    }
+
+    public void user(View v){
+        Intent helpIntent = new Intent(this, MainActivity.class);//Esta tiene que llevar a la de login alumno
+        startActivity(helpIntent);
+    }
+
+    public void professional(View v){
+        Intent helpIntent = new Intent(this, MainActivity.class);//Esta tiene que llevar a la de login profesional
+        startActivity(helpIntent);
     }
 }

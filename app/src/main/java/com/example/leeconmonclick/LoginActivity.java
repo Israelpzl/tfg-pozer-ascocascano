719374e6d6d8b137d4de7acad5089e5e03a10c40
Yeray <y.cornejo.2018@alumnos.urjc.es>
2022-03-24 18:56:39 +0100

    Changes manifest, fix minor error profiles screen and creation screen login

--- app/src/main/java/com/example/leeconmonclick/LoginActivity.java
+++ app/src/main/java/com/example/leeconmonclick/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.leeconmonclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void help(View v){
+        Intent helpIntent = new Intent(this, MainActivity.class);//Esta tiene que llevar a la de ayuda
+        startActivity(helpIntent);
     }
 }

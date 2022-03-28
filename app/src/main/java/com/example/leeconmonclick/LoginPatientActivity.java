b68426c7a14ce012673776fbf27345d623610b14
Israel Pozuelo <i.pozuelo.2018@alumnos.urjc.es>
2022-03-28 20:09:34 +0200

    Login-Patient
    Add LoginPatientActivity

---
+++ app/src/main/java/com/example/leeconmonclick/LoginPatientActivity.java
@@ -0,0 +1,14 @@
+package com.example.leeconmonclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_patient);
+    }
+}

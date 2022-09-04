ee68f0d37e349f1ba7d241e22fdbd8ffed178844
Israel Pozuelo <i.pozuelo.2018@alumnos.urjc.es>
2022-09-04 19:36:14 +0200

    save content in firebase

--- app/src/main/java/com/example/leeconmonclick/AddContentActivity.java
+++ app/src/main/java/com/example/leeconmonclick/AddContentActivity.java
@@ -3,9 +3,8 @@ package com.example.leeconmonclick;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -18,8 +17,28 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.example.leeconmonclick.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import es.leerconmonclick.util.Content;
+
 
 public class AddContentActivity extends AppCompatActivity {
 
@@ -27,6 +46,11 @@ public class AddContentActivity extends AppCompatActivity {
     private ImageView imageView;
     private ActivityResultLauncher<Intent> someActivityResultCamera;
     private ActivityResultLauncher<String> someActivityResultGalery;
+    private StorageReference storageReference;
+    private Uri uri;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth firebaseAuth;
+    private TextInputEditText word;
 
 
     @Override
@@ -35,8 +59,13 @@ public class AddContentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_content);
 
 
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
         spinner = (Spinner) findViewById(R.id.spinnerId);
         imageView = (ImageView) findViewById(R.id.imgViewId);
+        word = (TextInputEditText) findViewById(R.id.wordInputId);
 
         String [] opciones = {"-","El","La","Los","Las","Un","Una","Unos","Unas"};
 
@@ -48,6 +77,7 @@ public class AddContentActivity extends AppCompatActivity {
                 new ActivityResultCallback<Uri>() {
                     @Override
                     public void onActivityResult(Uri result) {
+                        uri = result;
                         imageView.setImageURI(result);
                     }
                 });
@@ -65,18 +95,46 @@ public class AddContentActivity extends AppCompatActivity {
                         }
                     }
                 });
-
     }
 
 
-    public void cameraImageBtn(View v){
-        someActivityResultCamera.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
-    }
+    public void cameraImageBtn(View v){ someActivityResultCamera.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE)); }
 
     public void galeryImageBtn(View v){
         someActivityResultGalery.launch("image/*");
     }
 
+    public void saveContent (View v){
 
+        if (uri != null){
+            StorageReference filePath = storageReference.child("contenidos").child(uri.getLastPathSegment());
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(getApplicationContext(),"Se ha subido la imagen",Toast.LENGTH_LONG).show();
+                }
+            });
+        }else{ Toast.makeText(getApplicationContext(),"Debe elegir una imagen",Toast.LENGTH_LONG).show(); }
+
+        List<String> sylablle = new ArrayList<>();
+        Content content = new Content(word.getText().toString(), uri.toString(),sylablle , spinner.getSelectedItem().toString());
+
+        databaseReference.child("content").child(word.getText().toString()).setValue(content).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(getApplicationContext(),"Se ha creado el contenido correctamente",Toast.LENGTH_LONG).show();
+                goProfile();
+            }
+        });
+
+
+
+    }
+
+    private void goProfile () {
+        Intent taskIntent = new Intent(this, ProfileActivity.class);
+        startActivity(taskIntent);
+        finish();
+    }
 
 }

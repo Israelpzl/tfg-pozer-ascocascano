43176c56824563fbebda1e92c49966e2fd30305c
Israel <israelpozuelo00@gmail.com>
2022-12-21 19:40:14 +0100

    fix dialogFragment

--- app/src/main/java/es/leerconmonclick/util/DialogSettingPatient.java
+++ app/src/main/java/es/leerconmonclick/util/DialogSettingPatient.java
@@ -2,17 +2,13 @@ package es.leerconmonclick.util;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
@@ -22,16 +18,16 @@ public class DialogSettingPatient extends AppCompatDialogFragment {
 
     private EditText EditNumber;
     private DialogListener listener;
-    private ImageButton checkBtn,cancelBtN;
+    private ImageButton checkBtn, cancelBtn;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_setting_patient,null);
 
         checkBtn = view.findViewById(R.id.checkBtnId);
-        cancelBtN = view.findViewById(R.id.cancelBtnId);
+        cancelBtn = view.findViewById(R.id.cancelBtnId);
         EditNumber = view.findViewById(R.id.number);
 
 
@@ -45,12 +41,14 @@ public class DialogSettingPatient extends AppCompatDialogFragment {
             }
         });
 
-        cancelBtN.setOnClickListener(new View.OnClickListener() {
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss();
             }
         });
+
+        builder.setView(view);
 
 
 

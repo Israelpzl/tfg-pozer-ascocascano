9072a81139bf1585a11851b11dafa8839fead1aa
Israel <israelpozuelo00@gmail.com>
2023-01-10 12:06:29 +0100

    add line in joinGame and opacity

--- app/src/main/java/com/example/leeconmonclick/patient/JoinWordsGameActivity.java
+++ app/src/main/java/com/example/leeconmonclick/patient/JoinWordsGameActivity.java
@@ -5,14 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
-import android.content.ClipData;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Pair;
-import android.view.DragEvent;
-import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,9 +18,7 @@ import android.widget.Toast;
 
 import com.example.leeconmonclick.R;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import es.leerconmonclick.util.LineView;
 
 public class JoinWordsGameActivity extends AppCompatActivity {
 
@@ -40,6 +36,11 @@ public class JoinWordsGameActivity extends AppCompatActivity {
     private int points;
     private AlertDialog alertDialog;
     private AlertDialog.Builder alertDialogBuilder;
+    private float cardImageX,cardImageY,cardWordX,cardWordY;
+    private Context context = this;
+    private boolean cardWordFinish1, cardWordFinish2, cardWordFinish3 = false;
+    private boolean cardImageFinish1, cardImageFinish2, cardImageFinish3 = false;
+    private int numSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,49 +60,65 @@ public class JoinWordsGameActivity extends AppCompatActivity {
     }
 
 
-
-    private void checkResult(){
-
-        if (img.equals(word)){
-            System.out.println("*****************Has ACERTADO*************");
-        }else{
-            System.out.println("*****************Has FALALDO*************");
-        }
-
-    }
-
     private void listenerClickSelect(){
 
         cardViewImage1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageSelect1.setVisibility(View.VISIBLE);
-                imageSelect2.setVisibility(View.INVISIBLE);
-                imageSelect3.setVisibility(View.INVISIBLE);
-                img = arrayImg[0];
-                select = true;
+                if (!cardImageFinish1){
+                    imageSelect1.setVisibility(View.VISIBLE);
+                    imageSelect2.setVisibility(View.INVISIBLE);
+                    imageSelect3.setVisibility(View.INVISIBLE);
+                    img = arrayImg[0];
+                    select = true;
+                    numSelect =1;
+
+                    int[] loc = new int[2];
+                    cardViewImage1.getLocationOnScreen(loc);
+                    cardImageX = loc[0];
+                    cardImageY = loc[1];
+                }
+
             }
         });
 
         cardViewImage2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageSelect1.setVisibility(View.INVISIBLE);
-                imageSelect2.setVisibility(View.VISIBLE);
-                imageSelect3.setVisibility(View.INVISIBLE);
-                img = arrayImg[1];
-                select = true;
+                if(!cardImageFinish2){
+                    imageSelect1.setVisibility(View.INVISIBLE);
+                    imageSelect2.setVisibility(View.VISIBLE);
+                    imageSelect3.setVisibility(View.INVISIBLE);
+                    img = arrayImg[1];
+                    select = true;
+                    numSelect =2;
+
+                    int[] loc = new int[2];
+                    cardViewImage2.getLocationOnScreen(loc);
+                    cardImageX = loc[0];
+                    cardImageY = loc[1];
+                }
+
             }
         });
 
         cardViewImage3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageSelect1.setVisibility(View.INVISIBLE);
-                imageSelect2.setVisibility(View.INVISIBLE);
-                imageSelect3.setVisibility(View.VISIBLE);
-                img = arrayImg[2];
-                select = true;
+                if (!cardImageFinish3){
+                    imageSelect1.setVisibility(View.INVISIBLE);
+                    imageSelect2.setVisibility(View.INVISIBLE);
+                    imageSelect3.setVisibility(View.VISIBLE);
+                    img = arrayImg[2];
+                    select = true;
+                    numSelect =3;
+
+                    int[] loc = new int[2];
+                    cardViewImage3.getLocationOnScreen(loc);
+                    cardImageX = loc[0];
+                    cardImageY = loc[1];
+                }
+
             }
         });
 
@@ -109,15 +126,50 @@ public class JoinWordsGameActivity extends AppCompatActivity {
             cardViewWord1.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (select) {
+
+                    if (select && !cardWordFinish1) {
                         wordSelect1.setVisibility(View.VISIBLE);
                         wordSelect2.setVisibility(View.INVISIBLE);
                         wordSelect3.setVisibility(View.INVISIBLE);
                         word = arrayWord [0];
                         if (img.equals(word)){
-                            Toast.makeText(getApplicationContext(), "Has Acertado", Toast.LENGTH_LONG).show();
                             points++;
-                            cardViewWord1.setVisibility(View.INVISIBLE);
+                            int[] loc = new int[2];
+                            cardViewWord1.getLocationOnScreen(loc);
+                            cardWordX = loc[0];
+                            cardWordY = loc[1];
+                            LineView lineView = new LineView(context,cardImageX+350,cardImageY+75,cardWordX-385,cardWordY+150);
+                            addContentView(lineView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+                            cardWordFinish1 = false;
+                            wordSelect1.setVisibility(View.INVISIBLE);
+
+
+                            float z = (float) 0.2;
+                            cardViewWord1.setAlpha(z);
+
+                            switch (numSelect){
+                                case 1:{
+                                    float a = (float) 0.2;
+                                    cardViewImage1.setAlpha(a);
+                                    cardImageFinish1 = true;
+                                    imageSelect1.setVisibility(View.INVISIBLE);
+                                    break;
+                                }
+                                case 2:{
+                                    float a = (float) 0.2;
+                                    cardViewImage2.setAlpha(a);
+                                    cardImageFinish2 = true;
+                                    imageSelect2.setVisibility(View.INVISIBLE);
+                                    break;
+                                } case 3: {
+                                    float a = (float) 0.2;
+                                    cardViewImage3.setAlpha(a);
+                                    cardImageFinish3 = true;
+                                    imageSelect3.setVisibility(View.INVISIBLE);
+                                    break;
+                                }
+                            }
+
                             if (points == 3){
                                 Toast.makeText(getApplicationContext(), "Juego Terminado", Toast.LENGTH_LONG).show();
                                 alertFinishGame();
@@ -133,17 +185,55 @@ public class JoinWordsGameActivity extends AppCompatActivity {
             });
 
             cardViewWord2.setOnClickListener(new View.OnClickListener() {
+                @SuppressLint("Range")
                 @Override
                 public void onClick(View v) {
-                    if (select) {
+                    if (select && !cardWordFinish2) {
                         wordSelect1.setVisibility(View.INVISIBLE);
                         wordSelect2.setVisibility(View.VISIBLE);
                         wordSelect3.setVisibility(View.INVISIBLE);
                         word = arrayWord [1];
                         if (img.equals(word)){
-                            Toast.makeText(getApplicationContext(), "Has Acertado", Toast.LENGTH_LONG).show();
                             points++;
-                            cardViewWord2.setVisibility(View.INVISIBLE);
+
+                            int[] loc = new int[2];
+                            cardViewWord2.getLocationOnScreen(loc);
+                            cardWordX = loc[0];
+                            cardWordY = loc[1];
+
+                            LineView lineView = new LineView(context,cardImageX+350,cardImageY+75,cardWordX-385,cardWordY+150);
+                            addContentView(lineView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+                            cardWordFinish2 = true;
+                            wordSelect2.setVisibility(View.INVISIBLE);
+
+                            float z = (float) 0.2;
+                            cardViewWord2.setAlpha(z);
+
+                            switch (numSelect){
+                                case 1:{
+                                    float a = (float) 0.2;
+                                    cardViewImage1.setAlpha(a);
+                                    cardImageFinish1 = true;
+                                    imageSelect1.setVisibility(View.INVISIBLE);
+                                    break;
+                                }
+                                case 2:{
+                                    float a = (float) 0.2;
+                                    cardViewImage2.setAlpha(a);
+                                    cardImageFinish2 = true;
+                                    imageSelect2.setVisibility(View.INVISIBLE);
+                                    break;
+                                } case 3: {
+                                    float a = (float) 0.2;
+                                    cardViewImage3.setAlpha(a);
+                                    cardImageFinish3 = true;
+                                    imageSelect3.setVisibility(View.INVISIBLE);
+                                    break;
+                                }
+                            }
+
                             if (points == 3){
                                 Toast.makeText(getApplicationContext(), "Juego Terminado", Toast.LENGTH_LONG).show();
                                 alertFinishGame();
@@ -160,15 +250,46 @@ public class JoinWordsGameActivity extends AppCompatActivity {
             cardViewWord3.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(select) {
+                    if(select && !cardWordFinish3) {
                         wordSelect1.setVisibility(View.INVISIBLE);
                         wordSelect2.setVisibility(View.INVISIBLE);
                         wordSelect3.setVisibility(View.VISIBLE);
                         word = arrayWord [2];
                         if (img.equals(word)){
-                            Toast.makeText(getApplicationContext(), "Has Acertado", Toast.LENGTH_LONG).show();
                             points++;
-                            cardViewWord3.setVisibility(View.INVISIBLE);
+                            int[] loc = new int[2];
+                            cardViewWord3.getLocationOnScreen(loc);
+                            cardWordX = loc[0];
+                            cardWordY = loc[1];
+                            LineView lineView = new LineView(context,cardImageX+350,cardImageY+75,cardWordX-385,cardWordY+150);
+                            addContentView(lineView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+                            cardWordFinish3 =true;
+                            wordSelect3.setVisibility(View.INVISIBLE);
+
+                            float z = (float) 0.2;
+                            cardViewWord3.setAlpha(z);
+                            switch (numSelect){
+                                case 1:{
+                                    float a = (float) 0.2;
+                                    cardViewImage1.setAlpha(a);
+                                    cardImageFinish1 = true;
+                                    imageSelect1.setVisibility(View.INVISIBLE);
+                                    break;
+                                }
+                                case 2:{
+                                    float a = (float) 0.2;
+                                    cardViewImage2.setAlpha(a);
+                                    cardImageFinish2 = true;
+                                    imageSelect2.setVisibility(View.INVISIBLE);
+                                    break;
+                                } case 3: {
+                                    float a = (float) 0.2;
+                                    cardViewImage3.setAlpha(a);
+                                    cardImageFinish3 = true;
+                                    imageSelect3.setVisibility(View.INVISIBLE);
+                                    break;
+                                }
+                            }
 
                             if (points == 3){
                                 Toast.makeText(getApplicationContext(), "Juego Terminado", Toast.LENGTH_LONG).show();

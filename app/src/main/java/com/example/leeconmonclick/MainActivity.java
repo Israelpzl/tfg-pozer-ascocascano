ffe9f07bef77250c222963282fc743a316cf25a1
Israel <israelpozuelo00@gmail.com>
2023-07-06 21:35:33 +0200

    change letras abc

--- app/src/main/java/com/example/leeconmonclick/MainActivity.java
+++ app/src/main/java/com/example/leeconmonclick/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.leeconmonclick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
@@ -9,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCenter.start(getApplication(), "3e7f65ac-796a-431c-94f1-ce9ba68cab98", Analytics.class, Crashes.class);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
